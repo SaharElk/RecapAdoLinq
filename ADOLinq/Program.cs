fe06f32c9070bda9cd48f0c495dc7e2ca7128c5a
SaharElk <sahar.elkhatt@gmail.com>
2021-06-04 05:24:07 -0700

    Debut step3

--- ADOLinq/Program.cs
+++ ADOLinq/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ADOLinq.Services;
+using System;
 using System.Data;
 using System.Data.SqlClient;
 using Tools.Connections.Database;
@@ -12,16 +13,7 @@ namespace ADOLinq
             // Test Connection
             Connection connection = new Connection(SqlClientFactory.Instance, @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ADOLinq;Integrated Security=True;");
 
-            Command command = new Command("SELECT Name FROM Category;", false);
-
-            DataTable dataTable = connection.GetDataTable(command);
-
-            foreach (DataRow dataRow in dataTable.Rows)
-            {
-                Console.WriteLine($"{dataRow["Name"]}");
-            }
-
-
+            CategoriesService categoriesService = new CategoriesService(connection);
         }
     }
 }
---
+++ ADOLinq/Services/CategoriesService.cs
@@ -0,0 +1,28 @@
+﻿using ADOLinq.Extensions;
+using ADOLinq.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tools.Connections.Database;
+
+namespace ADOLinq.Services
+{
+    public class CategoriesService
+    {
+        private readonly Connection _connection;
+
+        public CategoriesService(Connection connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<Category> Get()
+        {
+            Command command = new Command("SELECT Id, Name FROM Category;", false);
+
+            return _connection.ExecuteReader(command, (record) => record.ToCategory());
+        }
+    }
+}
