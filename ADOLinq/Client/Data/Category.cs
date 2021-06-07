65c6b5401000c93fbe9321853a70ef4ba00cbb04
SaharElk <sahar.elkhatt@gmail.com>
2021-06-07 04:31:14 -0700

    Category et Contact dans Data

---
+++ ADOLinq/Client/Data/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOLinq.Client.Data
+{
+    public class Category
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; }
+
+        public Category(string name)
+        {
+            Name = name;
+        }
+
+        internal Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ ADOLinq/Client/Data/Contact.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOLinq.Client.Data
+{
+    public class Contact
+    {
+        public int Id { get; private set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public int IdCategory { get; set; }
+
+        public Contact(string lastName, string firstName, string email, int idCategory)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            Email = email;
+            IdCategory = idCategory;
+        }
+
+        public Contact(int id, string lastName, string firstName, string email, int idCategory)
+        {
+            Id = id;
+            LastName = lastName;
+            FirstName = firstName;
+            Email = email;
+            IdCategory = idCategory;
+        }
+    }
+}
